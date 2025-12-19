c4cf9213a64c9ed2bf7d4eb22002047ddd238e67
FrancoisEricMerlin <francoiseric.merlin@paris.fr>
2025-12-19 14:52:12 +0100

    LUT-31054 : Add the possibility to manage sql updates with liquibase plugin

--- src/sql/plugins/genericattributes/plugin/init_db_generic_attributes_forms-cartography.sql
+++ src/sql/plugins/genericattributes/plugin/init_db_generic_attributes_forms-cartography.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
--- changeset genericattributes-cartography:init_db_carto_forms.sql
+-- changeset forms-cartography:init_db_generic_attributes_forms-cartography.sql
 -- preconditions onFail:MARK_RAN onError:WARN
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
 ('Cartographie',0,0,0,'forms.entryTypeCartography','map-marked-alt','forms',16,0);
